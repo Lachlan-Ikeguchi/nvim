fe88e02738db015129176434ab84a4a04e441d75
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2025-08-25 22:54:30 +1000

    forgot to write to the git-conflict.lua file

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -1,12 +1,3 @@
---[[
-co -- choose ours
-ct -- choose theirs
-cb -- choose both
-c0 -- choose none
-[x -- next conflict
-]x -- previous conflict
---]]
-
 return {
     'akinsho/git-conflict.nvim',
     version = "*",
