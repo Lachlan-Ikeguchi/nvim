188089dafbd4fe98c39db67d0a21ac27d6028517
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2026-07-05 20:18:04 +1000

    feat(matrix-theme): use main branch

--- lua/plugins/matrix-theme.lua
+++ lua/plugins/matrix-theme.lua
@@ -19,4 +19,4 @@ function apply_matrix_theme()
     require("ibl").setup { indent = { highlight = highlight } }
 end
 
-return { 'lachlan-ikeguchi/nvim-matrix-theme', branch = 'feat/expanded_palette' }
+return { 'lachlan-ikeguchi/nvim-matrix-theme', branch = 'main' }
